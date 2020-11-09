3db4a49f5a0d0ea2faa30b4e8d62e22f066fd992
Toby Lu <lutunye1311@gmail.com>
2020-11-09 23:08:45 +0800

    Add new file

---
+++ hi.py
@@ -0,0 +1,6 @@
+name = input('Hi user, can you tell me your name? ')
+print('Hi', name, 'nice to meet you.')
+weight = input('What is your weight(in kg)? ')
+print('You are', weight,'kg')
+height = input('Excellent, now how tall are you(in cm)? ')
+print('Well,', height, 'cm is not quite tall as I thought. JK!')
